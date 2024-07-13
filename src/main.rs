3ebc9e574493c22df7056cd12c672e3701d99d52
Ge0frey <jeffkimzyjoe@gmail.com>
2024-07-14 00:33:01 +0300

    Name shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-fn main() {
-    let mut x = 5;
-    x = 16;
-    println!("The value of x: {x}");  
+fn main () {
+    println!("Hello world");
+    print_x();
+}
+
+fn print_x () {
+    let  x = 50;
+    println!("The value of X is: {}", x);
+
+
+    {
+        let x = x - 100;
+        println!("The value of x is: {}", x);
+    }
+
+    let x = x + 1;
+    println!("The value of x is: {}", x);
 }
