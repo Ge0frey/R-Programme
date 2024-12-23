50cd8bfacdc58291f71d2412f4f5725d42fc3f04
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-23 13:59:14 +0300

    cargo new

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-fn main () {
+fn main() {
     println!("Hello world");
-    print_x();
-}
-
-fn print_x () {
-    let  x = 50;
-    println!("The value of X is: {}", x);
-
-
-    {
-        let x = x - 100;
-        println!("The value of x is: {}", x);
-    }
-
-    let x = x + 1;
-    println!("The value of x is: {}", x);
 }
