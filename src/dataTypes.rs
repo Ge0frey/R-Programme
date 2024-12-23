b73a2a56487df434fefa1f12f92be207bb46e84d
Ge0frey <jeffkimzyjoe@gmail.com>
2024-12-23 15:49:18 +0300

    scalar data types in rust

--- src/dataTypes.rs
+++ src/dataTypes.rs
@@ -1,4 +1,12 @@
 fn main () {
     //this file will handle data types in rust
-    let x: i 32 = 8; //implicit declaration of the data type this is an integer of size 32 bits
+    //scalar data types in rust
+    let x: i32 = 8; //explicit declaration of the data type this is a signed integer of size 32 bits
+
+    let floating_point: f32 = 22.6; //floating point values can either be f32 or f64;
+
+    let true_or_false: bool = true; //booleon values; can also be represented using 0(false) or 1(true);
+
+    let letter: char = 'c';
+
 }
